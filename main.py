d6512e1ef8820d034cbf0e511b86f5126a346007
KesterDaniel <kesterdan17@gmail.com>
2023-03-06 18:31:38 +0100

    sending email when price dips below lowest value

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+import os
+import smtplib
+import ssl
 from bs4 import BeautifulSoup
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 import requests
 from dotenv import load_dotenv
+
+load_dotenv()
+
+item_lowest_price = 540
 
 request_header = {
     "Accept-Language": "en-US,en;q=0.9",
@@ -18,3 +27,31 @@ webpage = response.text
 soup = BeautifulSoup(webpage, "html.parser")
 price_tag = soup.select_one(".a-price .a-offscreen")
 price = float(price_tag.getText().split("$")[1])
+item = soup.find(name="span", id="productTitle").getText()
+
+# Sending Email
+if price < item_lowest_price:
+    context = ssl.create_default_context()
+
+    EMAIL = "kesterdaniel401@gmail.com"
+    PASSWORD = os.getenv("EMAIL_PASSWORD")
+
+    msg = MIMEMultipart()
+    msg["from"] = EMAIL
+    msg["To"] = "kesterdan17@gmail.com"
+    msg["Subject"] = "Amazon Low Price Alert!!"
+
+    html = "<html><body>"
+    html += f"<h3>{item} now ${price}</h3>"
+    html += f"<p><a href={amazon_url}>Check it Out</a></p>"
+    html += "</body></html>"
+
+    msg.attach(MIMEText(html, "html"))
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", port=465, context=context) as connection:
+        connection.login(user=EMAIL, password=PASSWORD)
+        connection.sendmail(
+            from_addr=EMAIL,
+            to_addrs="kesterdan17@gmail.com",
+            msg=msg.as_string()
+        )
